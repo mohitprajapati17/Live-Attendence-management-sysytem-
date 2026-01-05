99d6ef9f4152ac72422f9b25d5466a0d487240b2
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-01-05 22:31:23 +0530

    attendence schema fix

---
+++ server/schema.ts/Attendence.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+
+const attendenceSchema = new mongoose.Schema({
+    classId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Class',
+        required: true
+    },
+    studentId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    status:{
+        type:String,
+        enum: ['present', 'absent']
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model('Attendence', attendenceSchema);
